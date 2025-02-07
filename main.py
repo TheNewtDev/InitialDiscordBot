c0190c89a6204b2ca92d15c02d7c17c5cac7df8d
Newton S <thenewtdev@gmail.com>
2025-02-06 18:12:58 -0700

    first functional bot change, includes listener for users

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+import discord
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
 
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+        if message.author == self.user:
+            return
+        if message.author.name == 'Vii' or message.author.name == 'vii':
+            print(f"Vii said something")
+            await message.channel.send(f'You\'re gay!')
+        if message.author.name == "_newtnewt":
+            print(f"Newt said something")
+            await message.channel.send(f'You\'re the best dad ever!')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+intents = discord.Intents.default()
+intents.message_content = True
 
+client = Client(intents=intents)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+client.run('token')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
